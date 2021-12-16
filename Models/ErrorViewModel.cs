abdda58b34bd8188fc96c983f2e4e5b1b822a6cf
Your Name <tuananh20417@gmail.com>
2021-12-16 07:36:18 +0700

    First_commit_NTA117

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NguyenTuanAnh117.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
